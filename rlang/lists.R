999ef49435f44de2f29fb4399b49f5845228f85b
joegasewicz <joegasewicz@gmail.com>
2026-03-25 17:49:18 +0000

    initial

--- rlang/lists.R
+++ rlang/lists.R
@@ -35,3 +35,35 @@ allList
 allList[[2]]$age # 20
 allList[[2]][[2]] # 20 - no item name
 allList[[2]][2] # maintains the name of the item
+
+### Double Square Brackets
+x <- list(name = "Adam", age = 35, didGraduate = T)
+x[1] # includes name
+x[[1]] # no name
+
+mode(x[1]) # list values
+mode(x[[1]]) # character value
+
+x[1:2]
+
+x[4] <- 170
+names(x)[4] <- "height"
+x
+
+x[5:7] <- c(T, T, F)
+x
+
+x[[8:10]] <- c(23, 34, 1) # will error!
+x
+
+# multiplying
+x[[4]]*2
+
+
+x <- list(name = "Adam", age = 35, didGraduate = T)
+x[[2]]*2
+
+x <- list(1,2,3,4)
+# x+1 # error!
+z <- unlist(x) + 1
+mode(z)
