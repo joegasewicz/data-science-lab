1f758927def426ddca5a1a3f68c52f45fc773e6c
joegasewicz <joegasewicz@gmail.com>
2026-03-25 17:34:13 +0000

    initial

--- rlang/lists.R
+++ rlang/lists.R
@@ -1,2 +1,37 @@
 # Lists
+# Store different variable modes in a single data structure.
+x <- list(name = "Adam", age = 35, didGraduate = T)
+mode(x)
+str(x)
+attributes(x)
+names(x)
+x
 
+xNameless <- list("Adam", 35, T)
+xNameless
+attributes(xNameless)
+
+xNameless[[1]]
+xNameless[[2]]
+xNameless[[3]]
+
+x$name
+x[["name"]]
+
+xNameless
+names(xNameless) <- c("Name", "age", "isGraduated")
+attributes(xNameless)
+xNameless$Name
+
+unlist(x) # ulist turns into a vector in to a single variable mode
+x
+
+x <- list(name = "Adam", age = 35, didGraduate = T)
+y <- list(name = "Sarah", age = 20, didGraduate = F)
+z <- list(name = "Bob", age = 40, didGraduate = F)
+allList = list(x, y, z)
+allList
+
+allList[[2]]$age # 20
+allList[[2]][[2]] # 20 - no item name
+allList[[2]][2] # maintains the name of the item
